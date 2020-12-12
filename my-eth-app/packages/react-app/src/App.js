537b057b1a4f62befc026fd29d45a11efe8bc2a1
masonicGIT <masonborda@gmail.com>
2020-12-12 12:31:00 -0600

    first commit

---
+++ my-eth-app/packages/react-app/src/App.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { Contract } from "@ethersproject/contracts";
+import { getDefaultProvider } from "@ethersproject/providers";
+import { useQuery } from "@apollo/react-hooks";
+
+import { Body, Button, Header, Image, Link } from "./components";
+import logo from "./ethereumLogo.png";
+import useWeb3Modal from "./hooks/useWeb3Modal";
+
+import { addresses, abis } from "@project/contracts";
+import GET_TRANSFERS from "./graphql/subgraph";
+
+async function readOnChainData() {
+  // Should replace with the end-user wallet, e.g. Metamask
+  const defaultProvider = getDefaultProvider();
+  // Create an instance of an ethers.js Contract
+  // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
+  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
+  // A pre-defined address that owns some CEAERC20 tokens
+  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
+  console.log({ tokenBalance: tokenBalance.toString() });
+}
+
+function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
+  return (
+    <Button
+	onClick={() => {
+          if (!provider) {
+            loadWeb3Modal();
+          } else {
+            logoutOfWeb3Modal();
+          }
+      }}
+    >
+    {!provider ? "Connect Wallet" : "Disconnect Wallet"}
+    </Button>
+  );
+}
+
+class Swapper extends React.Component {
+    constructor(props) {
+	super(props)
+	this.state = { isToggleOn: true }
+	this.handleClick =  this.handleClick.bind(this)
+	this.sushiSwap = "https://lite.sushiswap.fi/#/swap"
+        this.uniswap = "https://app.uniswap.org/#/swap?outputCurrency=0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359"
+    }
+
+    handleClick() {
+	this.setState(state => ({
+	    isToggleOn: !state.isToggleOn
+	}))
+    }
+    
+    render() {
+	return (
+	    <div>
+		 <iframe
+		     src={ this.state.isToggleOn ? this.uniswap : this.sushiSwap }
+		     width="100%"
+		     height="500"
+                     style={{ "border": "0", "margin": "0 auto", "display": "block", "border-radius": "10px", "max-width": "600px", "min-width": "300px" }}
+		     frameborder="0"
+		     allowfullscreen
+		     sandbox
+		 >
+		 </iframe>
+
+		<button onClick={this.handleClick}>
+		    {this.state.isToggleOn ? 'ON' : 'OFF'}
+		</button>
+	    </div>
+	)
+    }
+
+}
+
+function App() {
+  const { loading, error, data } = useQuery(GET_TRANSFERS);
+  const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
+
+  const [isActive, setActive] = useState("false")
+
+  const handleToggle = () => {
+    setActive(!isActive)
+  }
+    
+  React.useEffect(() => {
+    if (!loading && !error && data && data.transfers) {
+      console.log({ transfers: data.transfers });
+    }
+  }, [loading, error, data]);
+
+  return (
+    <div>
+      <Header>
+      <WalletButton provider={provider} loadWeb3Modal={loadWeb3Modal} logoutOfWeb3Modal={logoutOfWeb3Modal} />
+      </Header>
+	<Body>
+	<Swapper/>
+        <Button hidden onClick={() => readOnChainData()}>
+          Read On-Chain Balance
+        </Button>
+      </Body>
+    </div>
+  );
+}
+
+export default App;
